e3df327186e220b9c45976b8d66e1f55497b76b5
kefflen <kefflenjava@hotmail.com>
2024-02-17 20:38:02 -0300

    feat: create Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { IBM_Plex_Mono, Inter } from "next/font/google"
 import { ReactNode } from "react"
 import { ContactForm } from "./components/contact-form"
+import { Footer } from "./components/footer"
 import { Header } from "./components/header"
 import "./globals.css"
 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Header />
         {children}
         <ContactForm />
+        <Footer />
       </body>
     </html>
   )
