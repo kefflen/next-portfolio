2acfa579b6e1e3ddc88dea628f1c6bc7780a82f6
kefflen <kefflenjava@hotmail.com>
2024-02-20 09:53:44 -0300

    refactor: create a mock folder to put projects

--- app/(home)/_components/highlight-projects/index.tsx
+++ app/(home)/_components/highlight-projects/index.tsx
@@ -1,26 +1,9 @@
 import { HorizontalDevider } from "@/app/components/devider"
 import { Link } from "@/app/components/link"
 import { SectionTitle } from "@/app/components/section-title"
+import { mockProjects } from "@/app/mocks/projects"
 import { HiArrowNarrowRight } from "react-icons/hi"
 import { ProjectCard } from "./project-card"
-
-const MockProjects = [
-  {
-    title: "Flashcards App",
-    slug: "flashcard-app",
-    description:
-      "O projeto consiste em desenvolver um sistema de flash cards online que possa ser usado por educadores e estudantes para facilitar a memorização de conteúdos diversos. O sistema permite a criação de grupos de flash cards, que são cartões com uma pergunta e uma resposta, que devem ser revisados periodicamente. O sistema pode ser aplicado em diferentes áreas de conhecimento, como línguas estrangeiras, concursos públicos, entre outras. O objetivo do projeto é aproveitar as vantagens da tecnologia para tornar o processo de ensino e aprendizagem mais dinâmico e eficaz.",
-    thumbnailUrl: "/projects/flashcard/Thumbnail.png",
-    techs: [
-      "React",
-      "Express",
-      "Styled Components",
-      "Typescript",
-      "Node",
-      "Prisma",
-    ],
-  },
-]
 
 export const HighlightProjects = () => {
   return (
@@ -28,7 +11,7 @@ export const HighlightProjects = () => {
       <SectionTitle title="Projetos em destaque" subtitle="destaque" />
       <HorizontalDevider className="mb-16" />
       <div>
-        {MockProjects.map((project) => (
+        {mockProjects.map((project) => (
           <div key={project.slug}>
             <ProjectCard
               title={project.title}
---
+++ app/mocks/projects.ts
@@ -0,0 +1,18 @@
+
+export const mockProjects = [
+  {
+    title: "Flashcards App",
+    slug: "flashcard-app",
+    description:
+      "O projeto consiste em desenvolver um sistema de flash cards online que possa ser usado por educadores e estudantes para facilitar a memorização de conteúdos diversos. O sistema permite a criação de grupos de flash cards, que são cartões com uma pergunta e uma resposta, que devem ser revisados periodicamente. O sistema pode ser aplicado em diferentes áreas de conhecimento, como línguas estrangeiras, concursos públicos, entre outras. O objetivo do projeto é aproveitar as vantagens da tecnologia para tornar o processo de ensino e aprendizagem mais dinâmico e eficaz.",
+    thumbnailUrl: "/projects/flashcard/Thumbnail.png",
+    techs: [
+      "React",
+      "Express",
+      "Styled Components",
+      "Typescript",
+      "Node",
+      "Prisma",
+    ],
+  },
+]
