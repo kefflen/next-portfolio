5d64d602360f9f92bb22c258c117252e6e7b3644
kefflen <kefflenjava@hotmail.com>
2024-02-20 09:31:46 -0300

    feat: add flashcard project to highlights projects

--- app/(home)/_components/highlight-projects/index.tsx
+++ app/(home)/_components/highlight-projects/index.tsx
@@ -4,18 +4,42 @@ import { SectionTitle } from "@/app/components/section-title"
 import { HiArrowNarrowRight } from "react-icons/hi"
 import { ProjectCard } from "./project-card"
 
+const MockProjects = [
+  {
+    title: "Flashcards App",
+    slug: "flashcard-app",
+    description:
+      "O projeto consiste em desenvolver um sistema de flash cards online que possa ser usado por educadores e estudantes para facilitar a memorização de conteúdos diversos. O sistema permite a criação de grupos de flash cards, que são cartões com uma pergunta e uma resposta, que devem ser revisados periodicamente. O sistema pode ser aplicado em diferentes áreas de conhecimento, como línguas estrangeiras, concursos públicos, entre outras. O objetivo do projeto é aproveitar as vantagens da tecnologia para tornar o processo de ensino e aprendizagem mais dinâmico e eficaz.",
+    thumbnailUrl: "/projects/flashcard/Thumbnail.png",
+    techs: [
+      "React",
+      "Express",
+      "Styled Components",
+      "Typescript",
+      "Node",
+      "Prisma",
+    ],
+  },
+]
+
 export const HighlightProjects = () => {
   return (
     <section className="container py-16">
       <SectionTitle title="Projetos em destaque" subtitle="destaque" />
       <HorizontalDevider className="mb-16" />
       <div>
-        <ProjectCard />
-        <HorizontalDevider className="my-16" />
-        <ProjectCard />
-        <HorizontalDevider className="my-16" />
-        <ProjectCard />
-        <HorizontalDevider className="my-16" />
+        {MockProjects.map((project) => (
+          <div key={project.slug}>
+            <ProjectCard
+              title={project.title}
+              slug={project.slug}
+              description={project.description}
+              thumbnailUrl={project.thumbnailUrl}
+              techs={project.techs}
+            />
+            <HorizontalDevider className="my-16" />
+          </div>
+        ))}
         <p className="flex items-center gap-1.5">
           <span className="text-gray-400">Se interessou?</span>
           <Link href="/projects" className="">
--- app/(home)/_components/highlight-projects/project-card.tsx
+++ app/(home)/_components/highlight-projects/project-card.tsx
@@ -3,13 +3,26 @@ import { TechBadge } from "@/app/components/tech-badge"
 import Image from "next/image"
 import { HiArrowNarrowRight } from "react-icons/hi"
 
-type ProjectCardProps = {}
-export const ProjectCard = () => {
+type ProjectCardProps = {
+  title: string
+  slug: string
+  description: string
+  thumbnailUrl: string
+  techs: string[]
+}
+
+export const ProjectCard = ({
+  title,
+  slug,
+  description,
+  thumbnailUrl,
+  techs,
+}: ProjectCardProps) => {
   return (
     <div className="flex gap-6 lg:gap-12 flex-col lg:flex-row">
       <div className="w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full">
         <Image
-          src={"/images/placeholder-project.png"}
+          src={thumbnailUrl}
           alt={"Thumbnail do projeto"}
           width={420}
           height={304}
@@ -19,29 +32,23 @@ export const ProjectCard = () => {
       <div className="flex-1 lg:py-[18px]">
         <h3 className="flex items-center gap-3 font-medium text-lg text-gray-50">
           <Image
+            unoptimized
             src={"/images/icons/project-title-icon.svg"}
             width={20}
             height={20}
             alt={""}
           />
-          Nome do projeto
+          {title}
         </h3>
-        <p className="text-gray-400 my-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quae
-          numquam in suscipit. Rem nihil saepe quos omnis est! Ab asperiores,
-          amet vel magnam dicta nihil itaque corporis perferendis placeat.
-        </p>
+        <p className="text-gray-400 my-6">{description}</p>
 
         <div className="flex gap-x-2 gap-y-3 flex-wrap mb-8 lg:max-w-[350px]">
-          <TechBadge tech="React" />
-          <TechBadge tech="React" />
-          <TechBadge tech="React" />
-          <TechBadge tech="React" />
-          <TechBadge tech="React" />
-          <TechBadge tech="React" />
+          {techs.map((tech) => (
+            <TechBadge key={tech} tech={tech} />
+          ))}
         </div>
 
-        <Link href="/" className="">
+        <Link href={`/projects/${slug}`} className="">
           Ver projeto
           <HiArrowNarrowRight />
         </Link>
